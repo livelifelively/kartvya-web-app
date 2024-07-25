da1c4cf8ce4b3c1a3e4178db96ff55fd35ffc7a0
ashish rana <dev.livelifelively@gmail.com>
2024-07-26 04:01:52 +0530

    no sidesbar app-shell

---
+++ src/components/app-shell/no-sidebars-app-shell.tsx
@@ -0,0 +1,66 @@
+import Head from 'next/head';
+import { AppShell as AppShellCore, Burger, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+// import ServerOverload from '../error-pages/500';
+
+/* eslint-disable-next-line */
+export interface NoSideBarsAppShellProps {
+  children: React.ReactNode;
+  title: string;
+  footerType?: any;
+  error?: boolean;
+}
+
+export function NoSideBarsAppShell({
+  children,
+  title = 'Welcome to Qbeela!',
+  footerType,
+  error,
+}: NoSideBarsAppShellProps) {
+  // if (error) {
+  //   return <ServerOverload />;
+  // }
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="app">
+        <AppShellCore
+          styles={(theme: any) => ({
+            body: {
+              backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+              paddingBottom: rem(50),
+            },
+            main: {
+              paddingLeft: theme.spacing.md,
+              paddingRight: theme.spacing.md,
+              // [theme.fn.largerThan('md')]: {
+              //   paddingLeft: `calc(${theme.spacing.xl} * 1.5)`,
+              //   paddingRight: `calc(${theme.spacing.xl} * 1.5)`,
+              // },
+              paddingTop: theme.spacing.xl,
+              minHeight: 'unset',
+            },
+            root: {
+              // backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.brandBlue[0],
+              // minHeight: '100vh',
+            },
+          })}
+          layout="alt"
+          header={{ height: 60 }}
+        >
+          <AppShellCore.Header>
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+            <div>Logo</div>
+          </AppShellCore.Header>
+          {children}
+        </AppShellCore>
+      </main>
+    </>
+  );
+}
+
+export default NoSideBarsAppShell;
