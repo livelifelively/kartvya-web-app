bd13059a4d15dd5fc2a1493562979cad08fa4d0d
ashish rana <dev.livelifelively@gmail.com>
2024-07-26 04:56:08 +0530

    with badges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,47 @@
 import { ResponsiveAppShell } from '@/components/app-shell';
 import { NewsCard } from '@/components/news/news';
-import { Box } from '@mantine/core';
+import { Badge, Box, Flex, Indicator } from '@mantine/core';
 
 export default function HomePage() {
   return (
     <>
       <ResponsiveAppShell>
+        <Box>
+          <Flex
+            mih={50}
+            gap="xs"
+            justify="flex-start"
+            align="flex-start"
+            direction="row"
+            // wrap="wrap"
+          >
+            <Indicator>
+              <Badge variant="outline" color="gray" radius="xs">
+                Badge
+              </Badge>
+            </Indicator>
+            <Indicator>
+              <Badge variant="outline" color="gray" radius="xs">
+                Badge
+              </Badge>
+            </Indicator>
+            <Indicator>
+              <Badge variant="outline" color="gray" radius="xs">
+                Badge
+              </Badge>
+            </Indicator>
+            <Indicator>
+              <Badge variant="outline" color="gray" radius="xs">
+                Badge
+              </Badge>
+            </Indicator>
+            <Indicator>
+              <Badge variant="outline" color="gray" radius="xs">
+                Badge
+              </Badge>
+            </Indicator>
+          </Flex>
+        </Box>
         <Box mb="md">
           <NewsCard />
         </Box>
