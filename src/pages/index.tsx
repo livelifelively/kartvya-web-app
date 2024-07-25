b57400d38e27a35118ffb68e4d6fc8bede4d1df5
ashish rana <dev.livelifelively@gmail.com>
2024-07-26 04:26:49 +0530

    landing page init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import { ResponsiveAppShell } from '@/components/app-shell';
+import { NewsCard } from '@/components/news/news';
+import { Box } from '@mantine/core';
 
 export default function HomePage() {
   return (
     <>
-      <ResponsiveAppShell />
+      <ResponsiveAppShell>
+        <Box mb="md">
+          <NewsCard />
+        </Box>
+        <Box mb="md">
+          <NewsCard />
+        </Box>
+        <Box mb="md">
+          <NewsCard />
+        </Box>
+        <Box mb="md">
+          <NewsCard />
+        </Box>
+        <Box mb="md">
+          <NewsCard />
+        </Box>
+      </ResponsiveAppShell>
     </>
   );
 }
