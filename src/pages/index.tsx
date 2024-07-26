98b004d88a2f3ef0544443b7ee00870c98c54031
ashish rana <dev.livelifelively@gmail.com>
2024-07-26 14:06:47 +0530

    more modular

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,14 @@
 import { ResponsiveAppShell } from '@/components/app-shell';
-import { NewsCard } from '@/components/news/news';
+import { NewsListing } from '@/components/news-listing/news-listing';
+import { SubjectBadges } from '@/components/subject-badges/subject-badges';
 import { Badge, Box, Flex, Indicator } from '@mantine/core';
 
 export default function HomePage() {
   return (
     <>
       <ResponsiveAppShell>
-        <Box>
-          <Flex
-            mih={50}
-            gap="xs"
-            justify="flex-start"
-            align="flex-start"
-            direction="row"
-            // wrap="wrap"
-          >
-            <Indicator>
-              <Badge variant="outline" color="gray" radius="xs">
-                Badge
-              </Badge>
-            </Indicator>
-            <Indicator>
-              <Badge variant="outline" color="gray" radius="xs">
-                Badge
-              </Badge>
-            </Indicator>
-            <Indicator>
-              <Badge variant="outline" color="gray" radius="xs">
-                Badge
-              </Badge>
-            </Indicator>
-            <Indicator>
-              <Badge variant="outline" color="gray" radius="xs">
-                Badge
-              </Badge>
-            </Indicator>
-            <Indicator>
-              <Badge variant="outline" color="gray" radius="xs">
-                Badge
-              </Badge>
-            </Indicator>
-          </Flex>
-        </Box>
-        <Box mb="md">
-          <NewsCard />
-        </Box>
-        <Box mb="md">
-          <NewsCard />
-        </Box>
-        <Box mb="md">
-          <NewsCard />
-        </Box>
-        <Box mb="md">
-          <NewsCard />
-        </Box>
-        <Box mb="md">
-          <NewsCard />
-        </Box>
+        <SubjectBadges />
+        <NewsListing />
       </ResponsiveAppShell>
     </>
   );
