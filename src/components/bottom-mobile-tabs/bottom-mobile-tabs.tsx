6542a543d5942d4054a7b9c4e7d15e91da279a5e
Ashish Rana <dev.livelifelively@gmail.com>
2024-08-30 21:01:07 +0530

    new ui to test

--- src/components/bottom-mobile-tabs/bottom-mobile-tabs.tsx
+++ src/components/bottom-mobile-tabs/bottom-mobile-tabs.tsx
@@ -1,11 +1,31 @@
-import { Tabs } from '@mantine/core';
+import { Tabs, Text } from '@mantine/core';
+import classes from './bottom-mobile-tabs.module.css';
 
 export function BottomMobileTabs() {
   return (
-    <Tabs defaultValue="first" onChange={(value) => console.log(value)}>
+    <Tabs
+      inverted
+      defaultValue="first"
+      onChange={(value) => console.log(value)}
+      classNames={classes}
+    >
       <Tabs.List grow justify="center">
-        <Tabs.Tab value="first">Union</Tabs.Tab>
-        <Tabs.Tab value="second">State</Tabs.Tab>
+        <Tabs.Tab value="first">
+          <Text ta="center" fz="sm">
+            Union
+          </Text>
+          <Text ta="center" fz="xs">
+            Government
+          </Text>
+        </Tabs.Tab>
+        <Tabs.Tab value="second">
+          <Text ta="center" fz="sm">
+            State
+          </Text>
+          <Text ta="center" fz="xs">
+            Government
+          </Text>
+        </Tabs.Tab>
         <Tabs.Tab value="third">Citizens</Tabs.Tab>
         <Tabs.Tab value="forth">Profile</Tabs.Tab>
       </Tabs.List>
