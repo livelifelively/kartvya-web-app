c684fb965eacf68f87468ad12ca25866b710f8d0
Ashish Rana <dev.livelifelively@gmail.com>
2024-08-31 15:11:17 +0530

    fix build issues

--- src/test-utils/render.tsx
+++ src/test-utils/render.tsx
@@ -1,8 +1,10 @@
 import { render as testingLibraryRender } from '@testing-library/react';
 import { MantineProvider } from '@mantine/core';
-import { theme } from '../theme';
+import { useTheme } from '../theme';
 
 export function render(ui: React.ReactNode) {
+  const theme = useTheme();
+
   return testingLibraryRender(<>{ui}</>, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
       <MantineProvider theme={theme}>{children}</MantineProvider>
