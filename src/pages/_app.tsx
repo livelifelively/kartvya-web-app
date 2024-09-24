868c5e1bf9127a6899d1c50137fc26f2c6e9590f
ashish rana <dev.livelifelively@gmail.com>
2024-09-24 16:35:07 +0530

    with color scheme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import '@mantine/core/styles.css';
 import './global.css';
-// import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
-
 import { SessionProvider } from 'next-auth/react';
 import { useTheme } from '../theme';
 import AuthPageWrapper from '@/components/auth/auth-page-wrapper';
@@ -14,20 +12,29 @@ import {
 } from '@/components/color-scheme-context/color-scheme-context';
 import NextApp, { AppContext, AppProps } from 'next/app';
 import { useState } from 'react';
-import { getCookie, setCookie } from 'cookies-next';
+import { getCookie } from 'cookies-next';
 
-function CustomApp(props: AppProps & { colorScheme: ColorScheme }) {
+// Extend NextComponentType to include the auth property
+import { NextComponentType, NextPageContext } from 'next';
+
+type CustomAppProps = AppProps & {
+  colorScheme: ColorScheme;
+};
+
+type AuthComponentType = NextComponentType<NextPageContext, any, any> & {
+  auth?: boolean;
+};
+
+function CustomApp(props: CustomAppProps) {
   const {
     Component,
     pageProps: { session, ...pageProps },
   } = props;
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
-
-  const theme = useTheme(colorScheme);
+  const theme = useTheme();
 
   return (
     <SessionProvider session={session}>
-      <ColorSchemeProvider defaultColorScheme={colorScheme}>
+      <ColorSchemeProvider defaultColorScheme={props.colorScheme}>
         <MantineProvider theme={theme} defaultColorScheme="auto">
           <Notifications position="top-right" />
           <Head>
@@ -38,7 +45,7 @@ function CustomApp(props: AppProps & { colorScheme: ColorScheme }) {
             />
             <link rel="shortcut icon" href="/favicon.ico" />
           </Head>
-          {Component.auth ? (
+          {(Component as AuthComponentType).auth ? (
             <AuthPageWrapper>
               <Component {...pageProps} />
             </AuthPageWrapper>
@@ -60,39 +67,3 @@ CustomApp.getInitialProps = async (appContext: AppContext) => {
 };
 
 export default CustomApp;
-
-// function App({
-//   Component,
-//   pageProps: { session, ...pageProps },
-// }: {
-//   Component: any;
-//   pageProps: any;
-// }) {
-//   const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
-//   const theme = useTheme(colorScheme);
-
-//   return (
-//     <SessionProvider session={session}>
-//       <ColorSchemeProvider defaultColorScheme={colorScheme}>
-//         <MantineProvider theme={theme} defaultColorScheme="auto">
-//           <Notifications position="top-right" />
-//           <Head>
-//             <title>kartvya</title>
-//             <meta
-//               name="viewport"
-//               content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-//             />
-//             <link rel="shortcut icon" href="/favicon.ico" />
-//           </Head>
-//           {Component.auth ? (
-//             <AuthPageWrapper>
-//               <Component {...pageProps} />
-//             </AuthPageWrapper>
-//           ) : (
-//             <Component {...pageProps} />
-//           )}
-//         </MantineProvider>
-//       </ColorSchemeProvider>
-//     </SessionProvider>
-//   );
-// }
