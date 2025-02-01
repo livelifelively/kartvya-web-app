483eda50d0b42cfdef82a0f101223ecf83626a02
Ashish Rana <dev.livelifelively@gmail.com>
2025-02-01 16:52:37 +0530

    state and changes to the page

---
+++ src/components/onboarding/state/onboarding.tsx
@@ -0,0 +1,100 @@
+import { keyBy } from 'lodash';
+import { assign, setup } from 'xstate';
+
+interface StepperContext {
+  steps: number;
+  currentStep: number;
+}
+
+type StepperEvents = { type: 'E_NEXT' } | { type: 'E_PREVIOUS' };
+
+const stepperMachine = setup({
+  types: {
+    context: {} as StepperContext,
+    events: {} as StepperEvents,
+  },
+
+  actions: {
+    A_GO_TO_NEXT_STEP: assign(({ context, event }) => {
+      return {
+        currentStep: context.currentStep + 1,
+      };
+    }),
+
+    A_GO_TO_PREVIOUS_STEP: assign(({ context, event }) => {
+      return {
+        currentStep: context.currentStep - 1,
+      };
+    }),
+  },
+}).createMachine({
+  id: 'stepper',
+  initial: 'step1',
+  context: {
+    steps: 5, // Hardcoding for now, will make dynamic later
+    currentStep: 1, // keep track of the numeric value of current step
+  },
+  states: {
+    step1: {
+      on: {
+        E_NEXT: {
+          actions: [{ type: 'A_GO_TO_NEXT_STEP' }],
+          target: 'step2',
+        },
+      },
+    },
+    step2: {
+      on: {
+        E_NEXT: {
+          actions: [{ type: 'A_GO_TO_NEXT_STEP' }],
+          target: 'step3',
+        },
+        E_PREVIOUS: {
+          actions: [{ type: 'A_GO_TO_PREVIOUS_STEP' }],
+          target: 'step1',
+        },
+      },
+    },
+    step3: {
+      on: {
+        E_NEXT: {
+          actions: [{ type: 'A_GO_TO_NEXT_STEP' }],
+          target: 'step4',
+        },
+        E_PREVIOUS: {
+          actions: [{ type: 'A_GO_TO_PREVIOUS_STEP' }],
+          target: 'step2',
+        },
+      },
+    },
+    step4: {
+      on: {
+        E_NEXT: {
+          actions: [{ type: 'A_GO_TO_NEXT_STEP' }],
+          target: 'step5',
+        },
+        E_PREVIOUS: {
+          actions: [{ type: 'A_GO_TO_PREVIOUS_STEP' }],
+          target: 'step3',
+        },
+      },
+    },
+    step5: {
+      on: {
+        E_NEXT: {
+          actions: [{ type: 'A_GO_TO_NEXT_STEP' }],
+          target: 'done',
+        },
+        E_PREVIOUS: {
+          actions: [{ type: 'A_GO_TO_PREVIOUS_STEP' }],
+          target: 'step4',
+        },
+      },
+    },
+    done: {
+      type: 'final',
+    },
+  },
+});
+
+export default stepperMachine;
--- src/pages/citizen/welcome/index.tsx
+++ src/pages/citizen/welcome/index.tsx
@@ -1,10 +1,4 @@
-import { ResponsiveAppShell } from '@/components/app-shell';
-import { NavbarNested } from '@/components/app-shell/navbar-nested';
-import NoSideBarsAppShell from '@/components/app-shell/no-sidebars-app-shell';
-import { OnboardingAppShell } from '@/components/app-shell/onboarding-app-shell';
-import { NewsListing } from '@/components/news-listing/news-listing';
 import { Onboarding } from '@/components/onboarding/onboarding';
-// import { SubjectBadges } from '@/components/subject-badges/subject-badges';
 
 function WelcomePage() {
   return <Onboarding />;
