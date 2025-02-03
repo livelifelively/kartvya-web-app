2ac35ee4cc1822cf603ceaf5a6ba4ed7f3111ae8
Ashish Rana <dev.livelifelively@gmail.com>
2025-02-03 23:19:19 +0530

    onboarding stepper statemachine

--- src/components/onboarding/state/onboarding.state.tsx
+++ src/components/onboarding/state/onboarding.state.tsx
@@ -1,6 +1,7 @@
-import { setup } from 'xstate';
+import { assign, setup } from 'xstate';
 
 interface StepperContext {
+  currentStepIndex: number;
   selectSubjects: {
     allSubjects: string[];
     selectedSubjects: string[];
@@ -9,44 +10,89 @@ interface StepperContext {
 
 type StepperEvents = { type: 'E_NEXT' } | { type: 'E_PREVIOUS' };
 
+const statesMap = [
+  {
+    index: 0,
+    name: 'S_SELECT_SUBJECTS',
+    next: 'S_SELECT_REGION',
+  },
+  {
+    index: 1,
+    name: 'S_SELECT_REGION',
+    previous: 'S_SELECT_SUBJECTS',
+    next: 'S_DONE',
+  },
+  { index: 2, name: 'S_DONE' },
+];
+
+const initialState = statesMap[0].name;
+const updateStepActionCall = (step: any) => {
+  return {
+    type: 'A_UPDATE_STEP_INDEX',
+    params: () => {
+      return { currentStepIndex: step.index };
+    },
+  };
+};
+
+// first step can move forward, middle steps can move either way.
+// last step is final, it cannot move anywhere.
+const stepperMachineStates = statesMap.reduce((agg: any, val: any, index: number) => {
+  if (index === 0) {
+    agg[val.name] = {
+      entry: [updateStepActionCall(val)],
+      on: {
+        E_NEXT: {
+          target: val.next,
+        },
+      },
+    };
+  } else if (index === statesMap.length - 1) {
+    agg[val.name] = {
+      entry: [updateStepActionCall(val)],
+      type: 'final',
+    };
+  } else {
+    agg[val.name] = {
+      entry: [updateStepActionCall(val)],
+      on: {
+        E_NEXT: {
+          target: val.next,
+        },
+        E_PREVIOUS: {
+          target: val.previous,
+        },
+      },
+    };
+  }
+
+  return agg;
+}, {});
+
 const stepperMachine = setup({
   types: {
     context: {} as StepperContext,
     events: {} as StepperEvents,
   },
-
-  actions: {},
+  actions: {
+    A_UPDATE_STEP_INDEX: assign((_, params: { currentStepIndex: number }) => {
+      const { currentStepIndex } = params;
+      return {
+        currentStepIndex,
+      };
+    }),
+  },
 }).createMachine({
   id: 'stepper',
-  initial: 'S_SELECT_SUBJECTS',
+  initial: initialState,
   context: {
+    currentStepIndex: 0, // initial index
     selectSubjects: {
       allSubjects: [],
       selectedSubjects: [],
     },
   },
-  states: {
-    S_SELECT_SUBJECTS: {
-      on: {
-        E_NEXT: {
-          target: 'S_SELECT_REGION',
-        },
-      },
-    },
-    S_SELECT_REGION: {
-      on: {
-        E_NEXT: {
-          target: 'done',
-        },
-        E_PREVIOUS: {
-          target: 'S_SELECT_SUBJECTS',
-        },
-      },
-    },
-    done: {
-      type: 'final',
-    },
-  },
+  states: stepperMachineStates,
 });
 
 export default stepperMachine;
